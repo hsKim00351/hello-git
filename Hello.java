9a674e68a9de7a7615620a180d4eadffcaba37eb
hskim <1971462@hansung.ac.kr>
2020-11-23 22:59:43 +0900

    Fourth commit

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args){
         System.out.println("Hello, wold from GIT!");
         System.out.println("Chamges after First commit!")
+        System.out.println("Changes after Pushong to remote!");
     }
 }
